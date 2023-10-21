5c3cc76af872dd3c2b7eb310550d40f86f96eb68
winstxnhdw <winstxnhdw@gmail.com>
2023-10-21 13:45:10 +0800

    refactor: replace `envalid` with `zod`

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
-import { cleanEnv, str } from 'envalid'
+import { object, string } from 'zod'
 
-export const config = cleanEnv(Bun.env, {
-  COOKIE: str(),
-})
+export const config = object({
+  COOKIE: string().regex(/\.ASPXAUTH=/, {
+    message: 'COOKIE must contain a valid .ASPXAUTH cookie.',
+  }),
+}).parse(Bun.env)
