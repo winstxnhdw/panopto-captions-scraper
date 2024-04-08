279a20082c85b93f2e78a1a43af3cc0600274f99
winstxnhdw <winstxnhdw@gmail.com>
2024-04-08 23:43:50 +0800

    build: try migrating away from `esbuild`

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,6 @@
-import { cp, mkdir, rm } from 'node:fs/promises'
-import { build } from 'esbuild'
+import { mkdir, rm } from 'node:fs/promises'
 
 const build_directory = 'dist'
-const external_modules = [] as string[]
 
 async function main(args: string[]) {
   const arg = args.slice(2)[0]
@@ -10,26 +8,12 @@ async function main(args: string[]) {
   await rm(build_directory, { recursive: true, force: true })
   await mkdir(build_directory)
 
-  const results = await Promise.allSettled(
-    external_modules.map((module) =>
-      cp(`node_modules/${module}`, `${build_directory}/node_modules/${module}`, {
-        recursive: true,
-      }),
-    ),
-  )
-
-  for (const result of results) {
-    if (result.status !== 'rejected') continue
-    console.error(result.reason)
-  }
-
-  await build({
-    entryPoints: ['src/index.ts'],
-    outfile: `${build_directory}/index.js`,
-    bundle: true,
+  await Bun.build({
+    entrypoints: ['src/index.ts'],
+    outdir: `${build_directory}`,
+    target: 'bun',
+    format: 'esm',
     minify: arg !== '--test' && arg !== '-t',
-    platform: 'node',
-    external: external_modules,
   })
 }
 
